440ad12210bb637e9ec148b6a8b9dd2499cc278d
gacuguJohn <123625102+gacuguJohn@users.noreply.github.com>
2023-01-31 17:28:23 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var currentYear = new Date().getFullYear();
+currentYear=currentYear+1;
+const newYear = "1/1/"+currentYear;
+document.querySelector('.next_year').textContent=currentYear;
+
+const daysEl = document.querySelector('.day');
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.seconds');
+
+function timeCountDown() {
+  const nowDate = new Date();
+  const newYearDate = new Date(newYear);
+  const totalSeconds = (newYearDate - nowDate) / 1000;
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+
+  daysEl.textContent = formatTime(days);
+  hourEl.textContent = formatTime(hours);
+  minuteEl.textContent = formatTime(minutes);
+  secondEl.textContent = formatTime(seconds);
+}
+
+function formatTime(time) {
+  return time > 10 ? time : `0${time}`;
+}
+
+timeCountDown()
+setInterval(timeCountDown, 1000);
